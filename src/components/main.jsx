3595c50dc64b0ecef41afb2b42551bc9fefa2246
kunal.hedaoo <kunal.hedaoo@intellore.com>
2025-09-28 18:52:35 +0530

    adding skills component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,6 +2,7 @@ import { memo, use } from 'react';
 import Hero from './hero';
 import About from './about';
 import Stats from './stats';
+import Skills from './skills';
 import '../assets/css/main.css';
 
 const Main = () => {
@@ -11,6 +12,7 @@ const Main = () => {
        <Hero />
        <About />
        <Stats />
+       <Skills />
        </>
   );
 };
